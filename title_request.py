d16973f5816d25a73df9d589cfb23bc97105634d
azza1212 <jackicapatain44@outlook.com>
2025-01-10 09:55:49 +0800

    Update title_request.py

--- title_request.py
+++ title_request.py
@@ -2,7 +2,6 @@ import streamlit as st
 import discord
 import asyncio
 import threading
-import pyautogui
 import logging
 from cryptography.hazmat.primitives import serialization, hashes, padding
 from cryptography.hazmat.primitives.asymmetric import rsa
